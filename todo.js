7b5b33ac2fb9c30fb0d52465bfe5d1ca81ca3809
Gagan <icm2015003@iiita.ac.in>
2017-01-21 07:32:39 +0530

    files changed

--- todo.js
+++ todo.js
@@ -1,21 +1,24 @@
 var express = require('express')
 var cors = require('cors')
+var bodyParser = require('body-parser')
 var app = express()
 var tasks = [
 {
-	task: "eat"
+	term: "eat"
 	defined: "most important task of the day"
 
 },
 {
-	task: "sleep"
+	term: "sleep"
 	defined: "very important task"
 },
 {
-	task: "play"
+	term: "play"
 	defined: "very very important task"
 }
 ];
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended : false}))
 app.use(function(req,res,next){
 	console.log(`${req.method} request for ${req.url}`)
 	next()
@@ -23,6 +26,15 @@ app.use(function(req,res,next){
 app.use(express.static('/home/gaganjeet/Project1/public'))
 app.get('/task-api',function(req,res){
 	res.json(tasks)
+})
+app.post('/task-api',function(req,res){
+	tasks.push(req.body)
+	res.json(tasks)
+})
+app.delete('/task-api/:term',function(req,res){
+	tasks = tasks.filiter(function(definition){
+		return definition.term.toLowerCase()!= req.params.term.toLowerCase()
+			})
 })
 app.use(cors())
 app.listen(3003)
